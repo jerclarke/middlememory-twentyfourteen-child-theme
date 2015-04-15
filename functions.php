36203a3bf859eedd315c39537373216cd0c292d4
jeremyclarke <jer@simianuprising.com>
2015-04-14 20:12:12 -0400

    Initial import based on local dev of twentyfourteen child theme

---
+++ functions.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Functions.php for Middle Memory child theme based on Twenty Fourteen
+ */
+
+
+/**
+ * SARAH: Set this up as child theme of twentyfourteen
+ */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+/**
+ * SARAH: Fix attachment category/term listings to show attachments by fetching status=inherit instead of status=publish
+ */
+function wpa82573_show_tax_attachments( $query ) {
+    if (( $query->is_tax('attachment_category') OR $query->is_tax('attachment_tag') )&& $query->is_main_query() ) {
+        $query->set( 'post_status', 'inherit' );
+    }
+}
+add_action( 'pre_get_posts', 'wpa82573_show_tax_attachments' );
+
+
+function sarah_between_entry_header_and_content_geomashup_main_map() {
+	global $geo_mashup_options;
+	
+	if (!is_page())
+		return;
+
+	if (!class_exists('GeoMashupOptions') OR !isset($geo_mashup_options) OR !is_object($geo_mashup_options))
+		return;
+	
+	
+	$mashup_page_id = $geo_mashup_options->get( 'overall', 'mashup_page' );
+	
+	if (!$mashup_page_id OR is_wp_error($mashup_page_id))
+		return;
+	
+	
+	$current_page_id = get_queried_object_id();
+	
+	if ($current_page_id != $mashup_page_id)
+		return;
+
+	echo GeoMashup::map(array(
+		'map_content' => 'global',
+	));	
+}
+add_action('sarah_between_entry_header_and_content', 'sarah_between_entry_header_and_content_geomashup_main_map');
+
+/**
+ * SARAH - Filter wp_get_attachment_link to use $permalink=true mode on attachment taxonomy screens
+ * 
+ * By default attachments listed on archive screens are shown with wp_get_attachment_link() which 
+ * links to the file rather than attachment page on the image itself. 
+ * There is no direct filter to change the $permalink value to true to link to the attachment page.
+ * 
+ * Code in this function filters the output to re-run wp_get_attachment_link() with permalink=true 
+ * and replaces the output with that. 
+ * 
+ * It removes then re-adds itself before running to avoid an infinite loop
+ * 
+ * It only runs on attachment_category and attachment_tag archives to avoid breaking attachment
+ * display in other contexts.
+ */
+function sarah_filter_wp_get_attachment_link($output, $id, $size, $permalink, $icon, $text) {
+
+	// Only run on attachment taxonomy archives
+	if ( is_tax('attachment_category') OR is_tax('attachment_tag') ) :
+
+		// Remove this filter to avoid infinite loop
+		remove_filter('wp_get_attachment_link', 'sarah_filter_wp_get_attachment_link');
+	
+		// Re-run wp_get_attachment_link with permalink=true so it links to page instead of file
+		$permalink = true;
+		$output = wp_get_attachment_link($id, $size, $permalink, $icon, $text);
+		
+		// Re-add this filter for the next one
+		add_filter('wp_get_attachment_link', 'sarah_filter_wp_get_attachment_link', 10, 6);
+		
+	endif;
+
+	return $output;
+}
+add_filter('wp_get_attachment_link', 'sarah_filter_wp_get_attachment_link', 10, 6);
+
+/**
+ * SARAH: Filter geo mashup 'where' clause to insert 'inherit' as a valid status when attachments are being queried
+ *
+ * By default geo mashup queries for attachments when post_type=all but only fetches
+ * 'publish' so you never get any attachments' geo data
+ *
+ * NOTE: As of Geo Mashup 1.8.3 this hack isn't enough! We also need to edit GeoMashupQuery::generate_object_html()
+ * to insert 'inherit' as a valid status. 
+ * Unfortunately the plugin blocks access out of ignorance. It checks if posts are appropriate (i.e. if you asked for 
+ * future posts to be included) for the given query in the function that this filters, then on the query side it 
+ * just asks for all possible statuses. Unfortunately it forgot that 'inherit' is the only status for attachments
+ * and didn't include it even though attachments are a valid post type
+ * 
+ * @since Twenty Fourteen 1.0
+ */
+function sarah_filter_geo_mashup_locations_where($where) {
+
+  // Only filter if it's exactly what we expect Fetching attachments that are published (STUPID)
+  if (!strpos($where, "post_status = 'publish'") OR !strpos($where, "o.post_type IN ('post', 'page', 'attachment')"))
+    return $where;
+
+  // Add inherit
+  $where = str_replace("post_status = 'publish'", "post_status in( 'publish', 'inherit')", $where);
+
+  return $where;
+}
+add_filter( 'geo_mashup_locations_where', 'sarah_filter_geo_mashup_locations_where');
+
---
+++ image.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * The template for displaying image attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+// Retrieve attachment metadata.
+$metadata = wp_get_attachment_metadata();
+
+get_header();
+?>
+
+
+	<section id="primary" class="content-area image-attachment">
+		<div id="content" class="site-content" role="main">
+			<?php
+
+			?>
+	<?php
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
+	?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php 
+					/**
+					 * SARAH: Hide title and header entry-meta
+					 * NOTE: Cloned to post footer
+					 */
+					?>
+					<!--
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					<div class="entry-meta">
+
+						<span class="entry-date">Uploaded <time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time></span>
+
+						<span class="full-size-link"><a href="<?php echo esc_url( wp_get_attachment_url() ); ?>"><?php echo $metadata['width']; ?> &times; <?php echo $metadata['height']; ?></a></span>
+
+						<span class="parent-post-link"><a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>" rel="gallery"><?php echo get_the_title( $post->post_parent ); ?></a></span>
+						<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' ); ?>
+					</div>	
+					-->
+
+						<div class="entry-meta">
+
+						<?php
+
+						/**
+						 * SARAH: Show attachment categories and tags if they exist
+						 */
+						if (get_the_term_list( $post->ID, 'attachment_tag')) :
+							echo 'Attachment Tags: <ul class="attachment-tags">';
+							echo get_the_term_list( $post->ID, 'attachment_tag', '<li>', '</li><li>', '</li>' );
+							echo '</ul>';
+						endif;
+						if (get_the_term_list( $post->ID, 'attachment_category')) :
+							echo 'Attachment Categories: <ul class="attachment-categories">';
+							echo get_the_term_list( $post->ID, 'attachment_category', '<li>', '</li><li>', '</li>' );
+							echo '</ul>';
+						endif;
+?>
+
+
+
+			<span class="attachment-tag-links"><?php print_r( get_the_terms() )?></span>
+
+					</div><!-- .entry-meta -->
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<div class="entry-attachment">
+						<div class="attachment">
+							<?php twentyfourteen_the_attached_image(); ?>
+						</div><!-- .attachment -->
+
+						<?php if ( has_excerpt() ) : ?>
+						<div class="entry-caption">
+							<?php the_excerpt(); ?>
+						</div><!-- .entry-caption -->
+						<?php endif; ?>
+					</div><!-- .entry-attachment -->
+
+					<?php
+						the_content();
+						wp_link_pages( array(
+							'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+							'after'       => '</div>',
+							'link_before' => '<span>',
+							'link_after'  => '</span>',
+						) );
+					?>
+					<?php	
+					/**
+					 * Sarah: Show geo-mashup map
+					 */
+					if (class_exists('GeoMashup')) :
+						echo GeoMashup::map('single'); 
+					endif;
+					?>
+				</div><!-- .entry-content -->
+
+					<?php 
+					/**
+					 * SARAH: Clone entry-meta section from post header
+					 */
+					?>
+					<div class="entry-meta">
+					<!--
+						<span class="entry-date">Uploaded <time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time></span>
+					-->
+
+						<span class="full-size-link"><a href="<?php echo esc_url( wp_get_attachment_url() ); ?>"><?php echo $metadata['width']; ?> &times; <?php echo $metadata['height']; ?></a></span>
+						
+						<?php 
+						/**
+						 * SARAH: Fix bug that caused orphan attachments to link to themselves as parent. 
+						 * If post->post_parent was empty/zero get_permalink() returned global $post_id i.e. self
+						 * Making sure post_parent is true ensures we only link to parent post if there is one
+						 */
+						if ($post->post_parent):?>
+							<span class="parent-post-link">Part of <a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>" rel="gallery"><?php echo get_the_title( $post->post_parent ); ?></a></span>
+						<?php endif;?>	
+						<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' ); ?>
+					</div>				
+			</article><!-- #post-## -->
+
+			<nav id="image-navigation" class="navigation image-navigation">
+				<div class="nav-links">
+				<?php previous_image_link( false, '<div class="previous-image">' . __( 'Previous Image', 'twentyfourteen' ) . '</div>' ); ?>
+				<?php next_image_link( false, '<div class="next-image">' . __( 'Next Image', 'twentyfourteen' ) . '</div>' ); ?>
+				</div><!-- .nav-links -->
+			</nav><!-- #image-navigation -->
+
+			<?php comments_template(); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
